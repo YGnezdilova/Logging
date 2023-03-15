ee6912380afb4ee0af71e698f021d869dbf52e24
YGnezdilova <126925525+YGnezdilova@users.noreply.github.com>
2023-03-16 00:31:19 +0200

    Created New Car

--- src/main/java/org/example/CarTest.java
+++ src/main/java/org/example/CarTest.java
@@ -3,17 +3,17 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class CarTest {
-    @Test(groups = "create")
+    @Test(groups = {"create"})
     public void testCreateCar() {
         Car testCar =new Car("Honda", "Blue", 1973, 174);
     }
 
-    @Test(groups = "color")
+    @Test(groups = {"color"})
     public void testChangeColorOfCar() {
         Car testCar = new Car("Lexus", "Red", 3456, 200);
         testCar.SetColor("Blue");
     }
-    @Test(groups = "color")
+    @Test(groups = {"color"})
     public void testChangeColorToGold() {
         Car testCar = new Car("Mercedes-Benz", "Black", 3982, 200);
         testCar.SetColor("Gold");
